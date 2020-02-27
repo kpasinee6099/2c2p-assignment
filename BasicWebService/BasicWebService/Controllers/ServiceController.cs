0615514a92c92bbfc883cf3b92f72c748ca524e4
pasinee <pasinee>
2020-02-27 10:48:28 +0700

    initial project

---
+++ BasicWebService/BasicWebService/Controllers/ServiceController.cs
@@ -0,0 +1,26 @@
+﻿using BasicWebService.Models;
+using BasicWebService.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace BasicWebService.Controllers
+{
+    public class ServiceController : ApiController
+    {
+        
+        [HttpGet]
+        [Route("api/getCard", Name = "getCard")]
+        public bool getCard([FromUri]CardInfo tmpCard)
+        {
+            ValidateCard card = new ValidateCard();
+            bool isCom = card.isComplete(tmpCard);           
+            return isCom;
+        }
+
+
+    }
+}
