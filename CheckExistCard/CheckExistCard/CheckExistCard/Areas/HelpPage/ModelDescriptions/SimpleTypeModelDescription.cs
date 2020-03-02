14961d9a911c620c8ac6c7f3e1350af144c812cb
pasinee <pasinee>
2020-03-02 15:16:02 +0700

    delete unrelated projec

--- CheckExistCard/CheckExistCard/CheckExistCard/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace CheckExistCard.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
--- CheckExistCard/CheckExistCard/CheckExistCard/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CheckExistCard.WebApiApplication" Language="C#" %>
